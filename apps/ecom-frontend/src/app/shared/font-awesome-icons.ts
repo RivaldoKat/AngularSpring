af785e159043c3201322dc490b3077c3bac1324f
Rivaldo Sekome <rivaldokat25@gmail.com>
2025-01-15 17:15:39 +0200

    first commit

---
+++ apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,7 @@
+import { IconDefinition } from "@fortawesome/angular-fontawesome";
+import { faUser, faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
+export const fontAwesomeIcons: IconDefinition[] = [
+    faUser,
+    faCartShopping
+];
